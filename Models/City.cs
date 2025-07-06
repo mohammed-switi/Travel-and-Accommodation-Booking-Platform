22328e5567997b952da200962f8c3d24bc64881b
mohammed-switi <switi.2003@gmail.com>
2025-07-06 22:18:16 +0300

    Add City model

--- Models/City.cs
+++ Models/City.cs
@@ -1,6 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Final_Project.Models;
 
 public class City
 {
-    
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string Country { get; set; }
+
+    public string PostOffice { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public ICollection<Hotel> Hotels { get; set; }
 }
