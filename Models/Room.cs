e52faf35fb29b1bf7b9e42d3759555de5b034e5b
mohammed-switi <switi.2003@gmail.com>
2025-07-06 22:18:16 +0300

    Add Room model

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,6 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Final_Project.Models;
 
 public class Room
 {
-    
+    public int Id { get; set; }
+
+    public string RoomNumber { get; set; }
+
+    public decimal PricePerNight { get; set; }
+
+    public int Adults { get; set; }
+
+    public int Children { get; set; }
+
+    public bool IsAvailable { get; set; } = true;
+
+    [ForeignKey("Hotel")]
+    public int HotelId { get; set; }
+
+    public Hotel Hotel { get; set; }
+
+    public ICollection<BookingItem> BookingItems { get; set; }
 }
