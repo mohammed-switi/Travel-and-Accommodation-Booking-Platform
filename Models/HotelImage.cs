37336a84176698893c4d112afd4e0c8dfaa2a167
mohammed-switi <switi.2003@gmail.com>
2025-07-06 22:18:16 +0300

    Add HotelImage model

--- Models/HotelImage.cs
+++ Models/HotelImage.cs
@@ -2,5 +2,11 @@ namespace Final_Project.Models;
 
 public class HotelImage
 {
-    
+    public int Id { get; set; }
+
+    public string Url { get; set; }
+
+    public int HotelId { get; set; }
+
+    public Hotel Hotel { get; set; }
 }
