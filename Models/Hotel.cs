94f49cfb1bf38e35bbc6998e1d46c6261ee0a2dd
mohammed-switi <switi.2003@gmail.com>
2025-07-06 22:18:16 +0300

    Add Hotel model

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -1,6 +1,34 @@
-namespace Final_Project.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Final_Project.Models;
 public class Hotel
 {
-    
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public int StarRating { get; set; }
+
+    public string Owner { get; set; }
+
+    public string Location { get; set; }
+
+    [ForeignKey("City")]
+    public int CityId { get; set; }
+
+    public City City { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public ICollection<Room> Rooms { get; set; }
+
+    public ICollection<HotelImage> Images { get; set; }
+
+    public ICollection<Review> Reviews { get; set; }
 }
