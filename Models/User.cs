9621479239c2358e63f776c6606d5de018539196
mohammed-switi <switi.2003@gmail.com>
2025-07-06 22:18:16 +0300

    Add User model

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Final_Project.Models;
 
 public class User
 {
-    
+    public int Id { get; set; }
+
+    [Required, EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; }
+
+    [Required]
+    public string FullName { get; set; }
+
+    public string Role { get; set; } = "User"; // User or Admin
+
+    public ICollection<Booking> Bookings { get; set; }
+    public ICollection<RecentlyViewedHotel> RecentlyViewedHotels { get; set; }
 }
