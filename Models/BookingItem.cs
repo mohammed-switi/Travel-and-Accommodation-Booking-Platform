a280cd4f638944aeab50cc1c9831f12bdca25f84
mohammed-switi <switi.2003@gmail.com>
2025-07-06 22:20:12 +0300

    Add Booking Item Model

---
+++ Models/BookingItem.cs
@@ -0,0 +1,18 @@
+namespace Final_Project.Models;
+
+public class BookingItem
+{
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    public Room Room { get; set; }
+
+    public int BookingId { get; set; }
+
+    public Booking Booking { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Nights { get; set; }
+}
