d0c4cc30102ceda06445e8c4826d26819c194a49
mohammed-switi <switi.2003@gmail.com>
2025-07-06 22:18:16 +0300

    Add Review model

--- Models/Review.cs
+++ Models/Review.cs
@@ -2,5 +2,19 @@ namespace Final_Project.Models;
 
 public class Review
 {
-    
+    public int Id { get; set; }
+
+    public int HotelId { get; set; }
+
+    public Hotel Hotel { get; set; }
+
+    public int UserId { get; set; }
+
+    public User User { get; set; }
+
+    public int Rating { get; set; } // 1-5 stars
+
+    public string Comment { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
