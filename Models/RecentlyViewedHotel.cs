86a19a21eaa6f48b7ac47ca51dfd8a56198bc41b
mohammed-switi <switi.2003@gmail.com>
2025-07-06 22:18:16 +0300

    Add RecentlyViewedHotel model

--- Models/RecentlyViewedHotel.cs
+++ Models/RecentlyViewedHotel.cs
@@ -2,5 +2,15 @@ namespace Final_Project.Models;
 
 public class RecentlyViewedHotel
 {
-    
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public User User { get; set; }
+
+    public int HotelId { get; set; }
+
+    public Hotel Hotel { get; set; }
+
+    public DateTime ViewedAt { get; set; } = DateTime.UtcNow;
 }
