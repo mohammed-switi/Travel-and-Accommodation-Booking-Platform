65527ee91e07c7237d7876e7254d725e00d6b388
mohammed-switi <switi.2003@gmail.com>
2025-07-15 17:31:46 +0300

    Modify User model to contain fields related to forgot password feature

--- Models/User.cs
+++ Models/User.cs
@@ -6,17 +6,18 @@ public class User
 {
     public int Id { get; set; }
 
-    [Required, EmailAddress]
-    public string Email { get; set; }
+    [Required, EmailAddress] public string Email { get; set; }
 
-    [Required]
-    public string PasswordHash { get; set; }
+    [Required] public string PasswordHash { get; set; }
 
-    [Required]
-    public string FullName { get; set; }
+    [Required] public string FullName { get; set; }
 
     public string Role { get; set; } = "User"; // User or Admin
 
     public ICollection<Booking> Bookings { get; set; }
     public ICollection<RecentlyViewedHotel> RecentlyViewedHotels { get; set; }
+
+
+    public string? PasswordResetToken { get; set; }
+    public DateTime? PasswordResetTokenExpiration { get; set; }
 }
