1aa7448161d2246dbdb234b8d4f9e2e9111c2cab
mohammed-switi <switi.2003@gmail.com>
2025-07-15 19:02:18 +0300

    Create Home Contorller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Final_Project.Services;
+
 namespace Final_Project.Controllers;
 
-public class HomeController
+[ApiController]
+[Route("api/home/[controller]")]
+public class HomeController(IHomeService homeService) : ControllerBase
 {
-    
+    [HttpGet("featured-deals")]
+    public async Task<IActionResult> GetFeaturedDeals()
+    {
+        var deals = await homeService.GetFeaturedDealsAsync();
+        return Ok(deals);
+    }
+
+    [Authorize]
+    [HttpGet("recently-viewed")]
+    public async Task<IActionResult> GetRecentlyViewedHotels()
+    {
+        var user = HttpContext.User;
+        var hotels = await homeService.GetRecentlyViewedHotelsAsync(user);
+        return Ok(hotels);
+    }
+
+    [HttpGet("trending-destinations")]
+    public async Task<IActionResult> GetTrendingDestinations()
+    {
+        var destinations = await homeService.GetTrendingDestinationsAsync();
+        return Ok(destinations);
+    }
 }
