e756fb7b6ee7fcac1859af2c8e79f249dbd75f78
mohammed-switi <switi.2003@gmail.com>
2025-07-15 17:29:16 +0300

    Create Forgot Password DTO

--- DTOs/ForgotPasswordDto.cs
+++ DTOs/ForgotPasswordDto.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Final_Project.DTOs;
 
 public class ForgotPasswordDto
 {
-    
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; set; }
 }
