cd2ff2ef98c47e0235b05120b85e70a465a0d74d
mohammed-switi <switi.2003@gmail.com>
2025-07-15 21:25:43 +0300

    Create HotelSearchResutlDto

--- DTOs/HotelSearchResutlDto.cs
+++ DTOs/HotelSearchResutlDto.cs
@@ -1,6 +1,11 @@
 namespace Final_Project.DTOs;
 
-public class HotelSearchResutlDto
+public class HotelSearchResultDto
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public int StarRating { get; set; }
+    public string? ImageUrl { get; set; }
+    public decimal MinRoomPrice { get; set; }
 }
