ab0e79cdce886df766a77e26a9300552888dd7cc
mohammed-switi <switi.2003@gmail.com>
2025-07-15 22:32:50 +0300

    Migrate new changes

---
+++ Migrations/20250715193228_ModifyBookingItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Final_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyBookingItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckInDate",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutDate",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckInDate",
+                table: "BookingItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckOutDate",
+                table: "BookingItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckInDate",
+                table: "BookingItems");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutDate",
+                table: "BookingItems");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckInDate",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckOutDate",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
