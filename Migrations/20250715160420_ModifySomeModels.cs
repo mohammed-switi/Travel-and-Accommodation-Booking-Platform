804db3998640256698ca383979974969c92931c6
mohammed-switi <switi.2003@gmail.com>
2025-07-15 19:06:15 +0300

    Migrations to database

---
+++ Migrations/20250715160420_ModifySomeModels.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Final_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifySomeModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Discount",
+                table: "Rooms",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Hotels",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Hotels");
+        }
+    }
+}
