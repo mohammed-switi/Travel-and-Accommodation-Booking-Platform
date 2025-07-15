61cbeb2e7c8b5695e4f10ddc928282bef657ec71
mohammed-switi <switi.2003@gmail.com>
2025-07-15 17:29:01 +0300

    Create Login DTO

--- DTOs/LoginDto.cs
+++ DTOs/LoginDto.cs
@@ -1,6 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Final_Project.DTOs;
 
 public class LoginDto
 {
     
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
 }
