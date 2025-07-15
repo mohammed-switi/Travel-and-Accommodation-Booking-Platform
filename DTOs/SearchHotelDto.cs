974c4f8ed44a876cfcf121003bdaf636104c3aa9
mohammed-switi <switi.2003@gmail.com>
2025-07-15 21:26:02 +0300

    Create SearchHotleDto

--- DTOs/SearchHotelDto.cs
+++ DTOs/SearchHotelDto.cs
@@ -1,6 +1,20 @@
+using Final_Project.Enums;
+
 namespace Final_Project.DTOs;
 
-public class SearchHotelDto
+public class SearchHotelsDto
 {
-    
+    public string Location { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public int Adults { get; set; }
+    public int Children { get; set; }
+    public int Rooms { get; set; }
+
+    // Optional filters
+    public decimal? MinPrice { get; set; }
+    public decimal? MaxPrice { get; set; }
+    public int? StarRating { get; set; }
+    public List<Amenities>? Amenities { get; set; }
+    public List<RoomType>? RoomTypes { get; set; }
 }
