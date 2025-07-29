986a64441e3af8ebf6de47337f441fc1ee509fe1
mohammed-switi <switi.2003@gmail.com>
2025-07-29 23:49:45 +0300

    Create abstract methods for crud operations

---
+++ Interfaces/IRoomService.cs
@@ -0,0 +1,12 @@
+using Final_Project.DTOs;
+
+namespace Final_Project.Interfaces;
+
+public interface IRoomService
+{
+    Task<RoomDto> GetRoomByIdAsync(int id);
+    Task<List<RoomDto>> GetRoomsAsync(bool includeInactive = false);
+    Task<RoomDto> CreateRoomAsync(RoomDto roomDto);
+    Task<RoomDto> UpdateRoomAsync(int id, RoomDto roomDto);
+    Task<bool> DeleteRoomAsync(int id);
+}
