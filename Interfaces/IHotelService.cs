5dff62e7698a94f57c226c91c29791325915f415
mohammed-switi <switi.2003@gmail.com>
2025-07-29 22:45:18 +0300

    Add CRUD operation signatures to IHotelService interface

--- Interfaces/IHotelService.cs
+++ Interfaces/IHotelService.cs
@@ -4,6 +4,11 @@ namespace Final_Project.Services;
 
 public interface IHotelService
 {
+    Task<List<HotelDto>> GetHotelsAsync(bool includeInactive = false);
+    Task<HotelDto> GetHotelByIdAsync(int id);
+    Task<HotelDto> CreateHotelAsync(HotelDto hotelDto);
+    Task<HotelDto> UpdateHotelAsync(int id, HotelDto hotelDto);
+    Task<bool> DeleteHotelAsync(int id);
     Task<List<HotelSearchResultDto>> SearchHotelsAsync(SearchHotelsDto dto);
 
     Task<HotelDetailsDto> GetHotelDetailsAsync(int hotelId, DateTime? checkIn, DateTime? checkOut);
