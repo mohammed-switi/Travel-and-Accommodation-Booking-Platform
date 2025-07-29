b6b9abf212ff53596c88da45d611976bba65f02e
mohammed-switi <switi.2003@gmail.com>
2025-07-29 22:45:03 +0300

    Enhance HotelDto with description, timestamps, and amenities support

--- DTOs/HotelDto.cs
+++ DTOs/HotelDto.cs
@@ -1,5 +1,7 @@
-namespace Final_Project.DTOs;
+using Final_Project.Enums;
+using Final_Project.Models;
 
+namespace Final_Project.DTOs;
 
 public class HotelDto
 {
@@ -7,9 +9,16 @@ public class HotelDto
     public string Name { get; set; }
     public string City { get; set; }
     public string Location { get; set; }
+    public string Description { get; set; }
     public double StarRating { get; set; }
     public decimal OriginalPrice { get; set; }
     public decimal CurrentPrice { get; set; }
     public decimal Discount { get; set; }
     public string? ImageUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public ICollection<string> Images { get; set; }
+    public ICollection<ReviewDto> Reviews { get; set; }
+    public Boolean IsActive { get; set; } = true;
+    public Amenities Amenities { get; set; }
 }
