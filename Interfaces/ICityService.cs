e058f9e3eb8cbda3aec8a780cb3453a007c439da
mohammed-switi <switi.2003@gmail.com>
2025-07-30 21:14:03 +0300

    refactor: update ICityService interface for pagination
    - Modify GetCitiesAsync to accept page and pageSize parameters

--- Interfaces/ICityService.cs
+++ Interfaces/ICityService.cs
@@ -4,7 +4,7 @@ namespace Final_Project.Services;
 
 public interface ICityService
 {
-    Task<List<CityDto>> GetCitiesAsync();
+    Task<List<CityDto>> GetCitiesAsync(int page, int pageSize);
     Task<CityDto?> GetCityByIdAsync(int id);
     Task<CityDto> CreateCityAsync(CityDto cityDto);
     Task<CityDto> UpdateCityAsync(int id, CityDto cityDto);
