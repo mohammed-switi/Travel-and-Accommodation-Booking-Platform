ae12344d38c8c7b25e9c1ae35d2d881bb083b800
mohammed-switi <switi.2003@gmail.com>
2025-08-07 13:20:53 +0300

    refactor: update BookingCartItemDto structure

--- DTOs/BookingCartItemDto.cs
+++ DTOs/BookingCartItemDto.cs
@@ -4,8 +4,8 @@ public class BookingCartItemDto
 {
     public int Id { get; set; }
     public int RoomId { get; set; }
-    public string RoomName { get; set; }
-    public string HotelName { get; set; }
+    public string RoomType { get; set; }
+    public string? HotelName { get; set; }
     public DateTime CheckInDate { get; set; }
     public DateTime CheckOutDate { get; set; }
     public decimal Price { get; set; }
