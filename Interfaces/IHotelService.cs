8430f3fbc1a7941a98653a20eb4333dec4a7cae7
mohammed-switi <switi.2003@gmail.com>
2025-08-07 13:21:33 +0300

    refactor: update IHotelService interface with ownership methods

--- Interfaces/IHotelService.cs
+++ Interfaces/IHotelService.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Final_Project.DTOs;
 using Final_Project.DTOs.Requests;
 using Final_Project.DTOs.Responses;
@@ -7,10 +8,9 @@ namespace Final_Project.Services;
 public interface IHotelService
 {
     Task<List<HotelResponseDto>> GetHotelsAsync(int page, int pageSize, bool includeInactive = false);
-    Task<HotelResponseDto> GetHotelByIdAsync(int id);
+    Task<HotelResponseDto> GetHotelByIdAsync(int id,DateTime? checkIn, DateTime? checkOut ,ClaimsPrincipal? user = null);
     Task<HotelResponseDto> CreateHotelAsync(CreateHotelRequestDto hotelDto, int userId, string userRole);
     Task<HotelResponseDto> UpdateHotelAsync(int id, UpdateHotelRequestDto hotelDto, int userId, string userRole);
     Task<bool> DeleteHotelAsync(int id, int userId, string userRole);
     Task<List<HotelSearchResultDto>> SearchHotelsAsync(SearchHotelsDto dto);
-    Task<HotelResponseDto?> GetHotelDetailsAsync(int hotelId, DateTime? checkIn, DateTime? checkOut);
 }
