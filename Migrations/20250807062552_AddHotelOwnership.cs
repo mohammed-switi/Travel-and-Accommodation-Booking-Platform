b5d2f5faac4eeaaff4a2ff669d9da0a194afbfd8
mohammed-switi <switi.2003@gmail.com>
2025-08-07 13:21:54 +0300

    feat: add new migration for hotel ownership and reviews

---
+++ Migrations/20250807062552_AddHotelOwnership.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Final_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHotelOwnership : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookingCartItems_Rooms_RoomId",
+                table: "BookingCartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookingItems_Rooms_RoomId",
+                table: "BookingItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Users_UserId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Cities_CityId",
+                table: "Hotels");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Users_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "Owner",
+                table: "Hotels");
+
+            migrationBuilder.DropColumn(
+                name: "Nights",
+                table: "BookingItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "Hotels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostOffice",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SpecialRequests",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hotels_OwnerId",
+                table: "Hotels",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingCarts_UserId",
+                table: "BookingCarts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookingCartItems_Rooms_RoomId",
+                table: "BookingCartItems",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookingCarts_Users_UserId",
+                table: "BookingCarts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookingItems_Rooms_RoomId",
+                table: "BookingItems",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Users_UserId",
+                table: "Bookings",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Cities_CityId",
+                table: "Hotels",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Users_OwnerId",
+                table: "Hotels",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Users_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookingCartItems_Rooms_RoomId",
+                table: "BookingCartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookingCarts_Users_UserId",
+                table: "BookingCarts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookingItems_Rooms_RoomId",
+                table: "BookingItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Users_UserId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Cities_CityId",
+                table: "Hotels");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Users_OwnerId",
+                table: "Hotels");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Users_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Hotels_OwnerId",
+                table: "Hotels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookingCarts_UserId",
+                table: "BookingCarts");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Hotels");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Owner",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostOffice",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SpecialRequests",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Nights",
+                table: "BookingItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookingCartItems_Rooms_RoomId",
+                table: "BookingCartItems",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookingItems_Rooms_RoomId",
+                table: "BookingItems",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Users_UserId",
+                table: "Bookings",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Cities_CityId",
+                table: "Hotels",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Users_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
